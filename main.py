fc3eae2edd568bb16fa11271c9343f4ec7e76b67
g2nnyS <72686586+g2nnyS@users.noreply.github.com>
2022-09-15 21:47:04 +0800

    Webview

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-# -*- coding: utf-8 -*-
-#虚空终端
+from webssh.main import main
 
-#引入库，后续所有库在此引入
-import paramiko
-import time
-import sys
-import os
-import logging
-
-class SSHClient:
-    def __init__(self):
-        self.ssh_client = SSHClient()
+if __name__ == '__main__':
+    main()
 
--- run.py
+++ run.py
@@ -1,21 +1,24 @@
-from webssh.main import main
-
+import os
 import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
+
+os.popen('python main.py')
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        MainWindow.setWindowIcon(QIcon("./logo.ico"))
         self.setWindowTitle('虚空终端')  #窗口标题
         self.desktop = QApplication.desktop()
         self.setFixedSize(self.desktop.width() * 0.4, self.desktop.height() * 0.65) #窗口大小
         self.browser=QWebEngineView()
-        self.browser.load(QUrl('https://127.0.0.1:34223'))
+        self.browser.load(QUrl('http://127.0.0.1:34223'))
         self.setCentralWidget(self.browser)
 if __name__ == '__main__':
     app=QApplication(sys.argv)
     win=MainWindow()
     win.show()
     app.exit(app.exec_())
+    os.system('taskkill /im python.exe /f')
