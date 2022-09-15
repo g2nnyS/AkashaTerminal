da1c53ec29a369462216886054a623b15219db38
g2nnyS <72686586+g2nnyS@users.noreply.github.com>
2022-09-15 21:14:34 +0800

    删除一些没必要的文件

--- run.py
+++ run.py
@@ -1,5 +1,21 @@
 from webssh.main import main
 
-
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setWindowTitle('虚空终端')  #窗口标题
+        self.desktop = QApplication.desktop()
+        self.setFixedSize(self.desktop.width() * 0.4, self.desktop.height() * 0.65) #窗口大小
+        self.browser=QWebEngineView()
+        self.browser.load(QUrl('https://127.0.0.1:34223'))
+        self.setCentralWidget(self.browser)
 if __name__ == '__main__':
-    main()
+    app=QApplication(sys.argv)
+    win=MainWindow()
+    win.show()
+    app.exit(app.exec_())
