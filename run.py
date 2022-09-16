06ba55493777bc5e014c7301d77455091a72dc2c
tmdakm <96684075+tmdakm@users.noreply.github.com>
2022-09-16 22:44:17 +0800

    Update run.py
    Signed-off-by: tmdakm <96684075+tmdakm@users.noreply.github.com>

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ os.popen('python main.py')
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.setWindowTitle('虚空终端')  #窗口标题
+        self.setWindowTitle('虚空终端 | AkashaTerminal')  #窗口标题
         self.setWindowIcon(QIcon("./webssh/static/img/favicon.png"))
         self.desktop = QApplication.desktop()
         self.setFixedSize(self.desktop.width() * 0.4, self.desktop.height() * 0.65) #窗口大小
