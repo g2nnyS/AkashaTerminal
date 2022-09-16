504f9ec364a5294c1be7fa771443227dcc3a264d
g2nnyS <72686586+g2nnyS@users.noreply.github.com>
2022-09-17 00:13:18 +0800

    添加背景

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('虚空终端 | AkashaTerminal')  #窗口标题
         self.setWindowIcon(QIcon("./webssh/static/img/favicon.png"))
         self.desktop = QApplication.desktop()
-        self.setFixedSize(self.desktop.width() * 0.4, self.desktop.height() * 0.65) #窗口大小
+        self.setFixedSize(self.desktop.width() * 0.35, self.desktop.height() * 0.6) #窗口大小
         self.browser=QWebEngineView()
         self.browser.load(QUrl('http://127.0.0.1:34223'))
         self.setCentralWidget(self.browser)
