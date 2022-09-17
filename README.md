4e0991832d8c22b479ebcd5b78c6d357fa82feab
g2nnyS <72686586+g2nnyS@users.noreply.github.com>
2022-09-17 14:17:23 +0800

    修改了一点东西

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 <h1 align="center">虚空终端</h1>
  
 基于[WebSSH](https://github.com/huashengdun/webssh)修改的一款SSH工具，灵感来源于原神中的虚空终端
+
+## 开发与调试
+打开cmd或Shell,运行以下指令:
+```
+pip install Pyqt5
+```
+```
+pip install WebSSH
+```
--- run.py
+++ run.py
@@ -17,11 +17,39 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.setWindowTitle('虚空终端 | AkashaTerminal')  #窗口标题
         self.setWindowIcon(QIcon("./webssh/static/img/favicon.png"))
+        self.setWindowFlags(Qt.FramelessWindowHint)
         self.desktop = QApplication.desktop()
         self.setFixedSize(self.desktop.width() * 0.35, self.desktop.height() * 0.6) #窗口大小
         self.browser=QWebEngineView()
         self.browser.load(QUrl('http://127.0.0.1:34223'))
         self.setCentralWidget(self.browser)
+def _close_max_min_icon(self):
+    self.pushButton_3.setText('r')
+    self.pushButton_2.setText('1')
+    self.pushButton.setText('0')
+
+@pyqtSlot()
+def on_pushButton_clicked(self):
+    # 最小化
+    self.showMinimized()
+
+@pyqtSlot()
+def on_pushButton_2_clicked(self):
+    # 最大化与复原
+    if self.isMaximized():
+        self.showNormal()
+        self.pushButton_2.setText('1')  # 切换放大按钮图标字体
+        self.pushButton_2.setToolTip("<html><head/><body><p>最大化</p></body></html>")
+    else:
+        self.showMaximized()
+        self.pushButton_2.setText('2')
+        self.pushButton_2.setToolTip("<html><head/><body><p>恢复</p></body></html>")
+
+@pyqtSlot()
+def on_pushButton_3_clicked(self):
+    # 关闭程序
+    self.close()
+
 if __name__ == '__main__':
     app=QApplication(sys.argv)
     win=MainWindow()
